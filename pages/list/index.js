8bfc96fd9089ad2d00651f7953e5be1e830bf476
Ludloff, Johannes <johannes.ludloff@spark-radiance.eu>
2021-04-14 18:10:54 +0200

    dynamic routes

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,9 +1,31 @@
-const Test = () => {
+import styles from "../../styles/List.module.css";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+	return {
+		props: {
+			users: data,
+		},
+	};
+};
+
+const UserList = ({ users }) => {
 	return (
 		<div>
-			<h1>Test</h1>
+			{users &&
+				users.map((user) => {
+					return (
+						<Link href={"/list/" + user.id} key={user.id}>
+							<a className={styles.single}>
+								<strong>Name: {user.name} </strong>
+							</a>
+						</Link>
+					);
+				})}
 		</div>
 	);
 };
 
-export default Test;
+export default UserList;
